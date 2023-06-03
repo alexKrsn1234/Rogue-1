d9e8b68d87bf4279dc3eb2c74e7c1c4c333148e7
alexKrsn1234 <133713022+alexKrsn1234@users.noreply.github.com>
2023-06-03 15:13:03 +0200

    read me

--- Wearable.py
+++
@@ -1,8 +0,0 @@
-from Equipment import Equipment
-
-class Wearable(Equipment):
-    """A wearable equipment."""
-    def __init__(self, name, place, effect, abbrv="", use=None):
-        Equipment.__init__(self, name, abbrv, use)
-        self.place = place
-        self.effect = effect
